876ebb927d99a965443c1a9675eee831b28a9fbc
Jon Christie <jonpchristie@gmail.com>
2023-06-07 19:32:06 -0400

    Updates Blog

--- main.py
+++ main.py
@@ -7,15 +7,6 @@ from git import Repo
 
 from pathlib import Path
 
-PATH_TO_BLOG_REPO = Path('/Users/jonchristie/Desktop/WEB_DEV_DOCS/CLONED_REPOS/ai-blog/')
-
-PATH_TO_BLOG = PATH_TO_BLOG_REPO.parent
-
-
-PATH_TO_CONTENT = PATH_TO_BLOG/"content"
-
-
-PATH_TO_CONTENT.mkdir(exist_ok=True, parents=True)
 
 # create function for updating blog that adds anything new, commits it and pushes it
 # ALso create a function that sets up simple html file with inserts for actual call for whatever content we want to insert there
@@ -32,6 +23,15 @@ def update_blog(commit_message="Updates Blog"):
   origin.push()
 
 random_text_string = "!@#$M!F!#$~!!#$"
+PATH_TO_BLOG_REPO = Path('/Users/jonchristie/Desktop/WEB_DEV_DOCS/CLONED_REPOS/ai-blog/')
+
+PATH_TO_BLOG = PATH_TO_BLOG_REPO.parent
+
+
+PATH_TO_CONTENT = PATH_TO_BLOG/"content"
+
+
+PATH_TO_CONTENT.mkdir(exist_ok=True, parents=True)
 
 with open(PATH_TO_BLOG/"index.html", 'w') as f:
   f.write(random_text_string)
