4d164ea740be4ff5e28eb205208e07f6516af90f
Jon Christie <jonpchristie@gmail.com>
2023-06-07 22:00:23 -0400

    Updates Blog

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ import requests
 from PIL import Image
 
 # Set the API Key for OpenAI (If required)
-openai.api_key = "OPENAI_API_KEY"
+openai.api_key = os.getenv("OPENAI_API_KEY")
+print(openai.api_key)
 # Set path for blog repository
 PATH_TO_BLOG_REPO: Path = Path('/Users/jonchristie/Desktop/WEB_DEV_DOCS/CLONED_REPOS/ai-blog/')
 
